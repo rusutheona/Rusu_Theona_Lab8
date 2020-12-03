0dc21e4c7175bf474c1cf40cc7b0172768e4d5cc
THEONA RUSU <theona.rusu@stud.ubbcluj.ro>
2020-12-03 17:46:39 +0200

    Laborator 9

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -18,14 +18,33 @@ namespace Rusu_Theona_Lab8.Pages.Books
         {
             _context = context;
         }
+        public IList<Book> Book { get; set; }
+        public BookData BookD { get; set; }
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID)
+        {
+            BookD = new BookData();
 
-        public IList<Book> Book { get;set; }
-
-        public async Task OnGetAsync()
-        {   
-            Book = await _context.Book
-                .Include(b => b.Publisher)
-                .ToListAsync();
+            BookD.Books = await _context.Book
+            .Include(b => b.Publisher)
+            .Include(b => b.BookCategories)
+            .ThenInclude(b => b.Category)
+            .AsNoTracking()
+            .OrderBy(b => b.Title)
+            .ToListAsync();
+            if (id != null)
+            {
+                BookID = id.Value;
+                Book book = BookD.Books
+                .Where(i => i.ID == id.Value).Single();
+                BookD.Categories = book.BookCategories.Select(s => s.Category);
+            }
         }
+
+
+     
+
+        
     }
 }
